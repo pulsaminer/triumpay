87de6f33ae03b1e816e6ecebb7fbfb035c3ef6cb
pulsaminer <pulsaminer@gmail.com>
2025-09-04 21:08:48 +0700

    Delete download-spl-token.bat

--- download-spl-token.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Downloading updated SPL Token IIFE library...
-powershell -Command "Invoke-WebRequest -Uri 'https://unpkg.com/@solana/spl-token@0.3.8/lib/index.iife.min.js' -OutFile 'assets/js/libs/spl-token.iife.js'"
-echo Download complete!
-pause
