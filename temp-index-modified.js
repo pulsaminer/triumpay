3c2d6052b5f34765f2cc3ea97e02f6bcc272bfd1
pulsaminer <pulsaminer@gmail.com>
2025-09-04 21:10:14 +0700

    Delete temp-index-modified.js

--- temp-index-modified.js
+++
@@ -1,7 +0,0 @@
-import * as temp from './actions/index.js';
-import * as temp from './constants.js';
-import * as temp from './errors.js';
-import * as temp from './extensions/index.js';
-import * as temp from './instructions/index.js';
-import * as temp from './state/index.js';
-//# sourceMappingURL=index.js.map
